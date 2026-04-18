73d41c99dde6a509db28e3b24d798dc03776c7e0
iStarries <w18909622321@163.com>
2026-04-18 16:50:42 +0800

    init

--- sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
@@ -10,10 +10,10 @@ import java.io.Serializable;
 @ApiModel(description = "员工登录时传递的数据模型")
 public class EmployeeLoginDTO implements Serializable {
 
-    @ApiModelProperty("用户名")
+    @ApiModelProperty("请输入用户名")
     private String username;
 
-    @ApiModelProperty("密码")
+    @ApiModelProperty("请输入密码")
     private String password;
 
 }
